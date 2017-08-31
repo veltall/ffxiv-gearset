8aacffb3dc5dc90aa0eb8a57e0d949f907db6b87
Vu Dinh <vuniverse143@gmail.com>
2017-08-31 14:04:24 -0700

    implemented skill database in a single json file. Cleaned up raw files for memory purposes

--- db/xivdb-downloader.sh
+++ db/xivdb-downloader.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+# Takes a loooooooonnngggg time to download all 800+ json files
+# please don't use unless absolutely necessary
+header='https://api.xivdb.com/action/'
+
+for skill in $(ls ../images/res/raw); do
+    id=$(echo $skill | sed 's/_.*$//g')
+    URL=$header$id
+    curl $URL --output $skill.json
+done
