278e6bb9b4813d5b75d13e0f8e73c34333d0a1a2
Vu Dinh <vuniverse143@gmail.com>
2017-09-04 22:23:37 -0700

    beginning work on bypassing polyserve and loading files dynamically in Electron main.js

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function createWindow () {
   mainWindow.loadURL("http://localhost:8081/")
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
